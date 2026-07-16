d5a25aaaa2cdb602a53417ef0f8f2926bb5fd3f9
= <lachlan.miller.1990@outlook.com>
2026-07-16 19:42:11 +1000

    update

---
+++ bracken_build.sh
@@ -0,0 +1,5 @@
+bracken-build  \
+-d ../data/standard_8gb \
+-t 16 \
+-l 152 # illumina
+# -k ${KMER_LEN} 
---
+++ run_bracken.sh
@@ -0,0 +1,6 @@
+bracken \
+  -d ../data/standard_8gb \
+  -i sample.report \
+  -o sample.bracken \
+  -r 150 \
+  -l S
